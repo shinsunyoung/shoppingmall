741a87fd8cd04117f4be0ec4fe46605213b22c84
jongholee <jhleed3@gmail.com>
2019-05-05 14:24:32 +0900

    불필요 파일 제거

--- client/views/routes/indexRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const indexController = require('../controller/indexController');
-
-router.get('/', indexController.index);
-
-router.get('/about', (req, res, next) => res.send('created from Jongho Lee'));
-
-module.exports = router;
--- client/views/service/userService.js
+++
@@ -1,9 +0,0 @@
-const userRepository = require('../persistence/userRepository');
-
-function getUsers() {
-    return userRepository.getUsers();
-}
-
-module.exports = {
-    getUsers: getUsers
-};
