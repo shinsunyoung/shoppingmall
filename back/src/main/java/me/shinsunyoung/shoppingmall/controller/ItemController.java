8fdcc74d1f40c889e94b68afa9f810e168248807
Sunyoung Shin <s2017i30@e-mirim.hs.kr>
2019-05-06 18:46:26 +0900

    Shoppingmall-11 아이템 리스트 만들기 에러 고침

--- back/src/main/java/me/shinsunyoung/shoppingmall/controller/ItemController.java
+++ back/src/main/java/me/shinsunyoung/shoppingmall/controller/ItemController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,33 +16,8 @@ public class ItemController {
 
     @GetMapping("/item")
     public List<Item> findItems(){
-//       return itemService.findItems();
+       return itemService.findItems();
 
-
-        List<Item> result = new ArrayList<>();
-        Item item1 = new Item();
-
-        item1.setId(1l);
-        item1.setName("아이스티");
-        item1.setPrice(2000);
-        item1.setSellerId(2l);
-        item1.setCategoryId(1);
-        item1.setStatus("ON");
-
-        result.add(item1);
-
-        Item item2 = new Item();
-
-        item2.setId(2l);
-        item2.setName("아메리카노");
-        item2.setPrice(1500);
-        item2.setSellerId(2l);
-        item2.setCategoryId(1);
-        item2.setStatus("ON");
-
-        result.add(item2);
-
-        return result;
     }
 
 
